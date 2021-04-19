8f7b4a45749cea15bd6366f2e6178e6fdb4c2d53
alizhomart <180107186@stu.sdu.edu.kz>
2021-04-20 00:00:36 +0600

    labka4-5

---
+++ src/main/java/com/example/labka45/Labka45Application.java
@@ -0,0 +1,13 @@
+package com.example.labka45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labka45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labka45Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/labka45/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.labka45.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+}
